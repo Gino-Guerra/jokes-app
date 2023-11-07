81240acdb8cb39dde0e6b29fe9097565d0fda485
Gino Guerra <ginoiguerra@gmail.com>
2023-11-07 12:43:15 -0500

    joke app just need to shuffle the jokes

---
+++ styles.js
@@ -0,0 +1,49 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const { width } = Dimensions.get('screen')
+
+
+
+const styles = StyleSheet.create({
+container:{
+    flex:1,
+    backgroundColor: '#cc0',
+    justifyContent: "space-around",
+    alignContent: "center",
+},
+bigText:{
+    fontSize: 40,
+    fontWeight: '700',
+    color: '#d00',
+    textAlign: 'center'
+
+},
+punchlineTextHidden:{
+fontSize:30,
+color: 'white'
+},
+punchlineBoxHidden:{
+    backgroundColor: '#00d',
+    width: width,
+    height: 104,
+    justifyContent: "center",
+    alignItems: "center"
+},
+punchlineText:{
+fontSize:30,
+color: '#00d'
+},
+punchlineBox:{
+   
+    width: width,
+    height: 104,
+    justifyContent: "center",
+    alignItems: "center"
+}
+
+
+
+
+})
+
+export default styles
